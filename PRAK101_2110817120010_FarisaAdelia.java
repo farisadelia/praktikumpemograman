b77451cda83d0f04f7658b07721cefef636d7dc2
Farisa Adelia <115018384+farisadelia@users.noreply.github.com>
2022-10-05 05:04:32 +0800

    Update PRAK101_2110817120010_FarisaAdelia.java

--- PRAK101_2110817120010_FarisaAdelia.java
+++ PRAK101_2110817120010_FarisaAdelia.java
@@ -3,28 +3,63 @@ import java.util.Scanner;
 
 public class PRAK105_2110817120010_FarisaAdelia {
 public static void main(String[] args) {
-        //Konstanta PHI
-        final float PHI = 3.14f;
 
-        /* Variable:
-        r untuk jari-jari
-        t untuk tinggi dan volume
-         */
-        float r, t, volume;
+        //Scanner untuk menerima input dari user
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("=====INPUT=====");
-        System.out.print("Masukan jari-jari \t : ");
-        r = keyboard.nextFloat();
+        //Deklerasi Variabel
+        String namaLengkap, tempatLahir;
+        int tanggalLahir, bulanLahir, tahunLahir;
+        float beratBadan, tinggiBadan;
+        String namaBulan = null;
 
-        System.out.print("Masukan tinggi \t\t : ");
-        t = keyboard.nextFloat();
+        //Input Nama
+        System.out.print("Masukan Nama Lengkap: ");
+        namaLengkap = keyboard.nextLine();
 
-        //Rumus volume tabung
-        volume = PHI * r * r * t;
+        //Input Tempat Lahir
+        System.out.print("Masukan Tempat Lahir: ");
+        tempatLahir = keyboard.nextLine();
+
+        //Input Tempat Tanggal Lahir:
+        System.out.print("Masukan Tanggal Lahir: ");
+        tanggalLahir = keyboard.nextInt();
+
+        //Input Bulan lahir
+        System.out.print("Masukan Bulan Lahir: ");
+        bulanLahir = keyboard.nextInt();
+        //Logika numerik ke nama bulan
+        if (bulanLahir == 1) namaBulan = "Januari";
+        else if (bulanLahir == 2) namaBulan = "Februari";
+        else if (bulanLahir == 3) namaBulan = "Maret";
+        else if (bulanLahir == 4) namaBulan = "April";
+        else if (bulanLahir == 5) namaBulan = "Mei";
+        else if (bulanLahir == 6) namaBulan = "Juni";
+        else if (bulanLahir == 7) namaBulan = "Juli";
+        else if (bulanLahir == 8) namaBulan = "Agustus";
+        else if (bulanLahir == 9) namaBulan = "September";
+        else if (bulanLahir == 10) namaBulan = "Oktober";
+        else if (bulanLahir == 11) namaBulan = "November";
+        else if (bulanLahir == 12) namaBulan = "Desember";
+
+        //Input Tahun lahir
+        System.out.print("Masukan Tahun Lahir: ");
+        tahunLahir = keyboard.nextInt();
+
+        //Input Tinggi badan
+        System.out.print("Masukan Tinggi badan: ");
+        tinggiBadan = keyboard.nextFloat();
+
+        //Input Berat badan
+        System.out.print("Masukan Berat badan: ");
+        beratBadan = keyboard.nextFloat();
+
+        keyboard.close();
 
         //Output
-        System.out.println("=====OUTPUT=====");
-        System.out.printf("Volume tabung dengan jari-jari %.1f cm dan tinggi %.1f cm adalah %.3f m3",r,t,volume);
+        System.out.println("Data Telah Ditambahkan,");
+        System.out.println("Nama Lengkap "+ namaLengkap + ", Lahir di "+ tempatLahir + " pada Tanggal " + tanggalLahir +" "+ namaBulan + " "+ tahunLahir +
+                "\nTinggi Badan " + tinggiBadan + " cm dan Berat Badan " +beratBadan+ " kilogram");
+
     }
-}   
+}
